57f4700ec4377c4638ed1b5a4c547be5ad909ca2
Zach Murray <zachary@nirgal.io>
2025-10-24 19:10:09 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+pub mod browser_constroller;
+pub mod execution_engine;
+pub mod models;
+pub mod output;
+pub mod state_capture;
+pub mod task_definition;
+
+fn main() {
+  println!("Hello, world!");
+}
